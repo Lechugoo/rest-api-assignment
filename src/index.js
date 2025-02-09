cddcede8a31d86ed3ad9a5509038a8b7baadc3ef
iosif <iam@njit.edu>
2025-02-09 18:36:37 -0500

    iosif moraru1

--- src/index.js
+++ src/index.js
@@ -9,9 +9,50 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+
+
+
+let users = {}; 
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return res.status(400).json({ message: 'Need name and email!' });
+    }
+    const id = Date.now().toString();
+    users[id] = { id, name, email }; 
+    console.log("User created successfully! ", users[id]);
+    res.status(201).json(users[id]);
 });
+
+app.get('/users/:id', (req, res) => {
+    const user = users[req.params.id];
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return res.status(400).json({ message: 'Need name and email!' });
+    }
+    if (!users[req.params.id]) {
+        return res.status(404).json({ message: 'user not found!!' });
+    }
+    users[req.params.id] = { ...users[req.params.id], name, email };
+    res.json(users[req.params.id]);
+});
+
+app.delete('/users/:id', (req, res) => {
+    if (!users[req.params.id]) {
+        return res.status(404).json({ message: 'user not found!' });
+    }
+    delete users[req.params.id]; 
+    res.status(204).send();
+});
+
 
 // Do not touch the code below this comment
 // **************************************************************
